da41a003eb3ac4bcc26ad902da1fa4a3324b69d4
Dell <mahak.agrawal_cs19@gla.ac.in>
2020-08-29 20:45:50 +0530

    create getters and setters for Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -7,17 +7,26 @@
 package definitions;
 
 public class Library {
-    private Book [] books;
+    private Book[] books;
 
     //constructor
-    public Library(){
-        books =new Book[5];
-        for (int i = 0; i <books.length ; i++) {
+    public Library() {
+        books = new Book[5];
+        for (int i = 0; i < books.length; i++) {
             books[0] = new Book();
             books[1] = new Book("Book");
             books[2] = new Book(44);
-            books[3] = new Book("Book Author","Book Name");
-            books[4] = new Book("Book Name","Book Author","Book Number");
+            books[3] = new Book("Book Author", "Book Name");
+            books[4] = new Book("Book Name", "Book Author", "Book Number");
         }
+    }
+
+    //getters and setters method
+    public Book[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(Book[] books) {
+        this.books = books;
     }
 }
