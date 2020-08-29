9a9de2115e3f29b59375d9916fe6cd4f9ac6c4e0
Dell <mahak.agrawal_cs19@gla.ac.in>
2020-08-29 22:01:18 +0530

    call the addBook() method from the menu and call the listOfAvailableBooks() method

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -19,12 +19,35 @@ public class FrontDesk {
     private static final int EXIT = 4;
 
     public static void main(String[] args) {
-        Library library = new Library();
-        Student student = new Student();
         Byte studentInput;
         String bookName;
         String studentName;
         Long studentUniversityRollNumber;
+        Library library = new Library();
+        Student student = new Student();
         Scanner scanner = new Scanner(System.in);
-    }
-}
+        do {
+            System.out.println("Hello," + student.getStudentName() + "!!");
+            System.out.println("-=-=--=-=-Welcome To The Front Desk-=-=--=-=-");
+            System.out.println("How may I help today?");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return a previously issues book for me.");
+            System.out.println("3. Show me all my issues books.");
+            System.out.println("4. Exit.");
+            System.out.println("Enter your choice (1..4): ");
+            studentInput = scanner.nextByte();
+            switch (studentInput) {
+                case ADD_BOOK:
+                    System.out.println("Enter your name:");
+                    studentName = scanner.nextLine();
+                    System.out.println("Enter your university roll number:");
+                    studentUniversityRollNumber = scanner.nextLong();
+                    System.out.println("These are the books currently available in library:");
+                    library.listOfAvailableBooks();
+                    System.out.println("Enter the name of the book that you want to issue:");
+                    scanner.nextLine();
+                    bookName = scanner.nextLine();
+                    student.addBook(bookName);
+                    break;
+            }
+        }
