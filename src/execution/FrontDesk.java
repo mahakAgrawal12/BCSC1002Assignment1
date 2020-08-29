75d7a86b9c6f3159ef1233aea8b7c2312e641dfd
Dell <mahak.agrawal_cs19@gla.ac.in>
2020-08-29 21:53:57 +0530

    create objects for Library and  Student class in FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,8 +6,25 @@
  * */
 package execution;
 
-public class FrontDesk {
-    public static void main(String[] args) {
+import definitions.Library;
+import definitions.Student;
 
+import java.util.Scanner;
+
+public class FrontDesk {
+
+    private static final int ADD_BOOK = 1;
+    private static final int RETURN_BOOK = 2;
+    private static final int ISUUE_BOOK = 3;
+    private static final int EXIT = 4;
+
+    public static void main(String[] args) {
+        Library library = new Library();
+        Student student = new Student();
+        Byte studentInput;
+        String bookName;
+        String studentName;
+        Long studentUniversityRollNumber;
+        Scanner scanner = new Scanner(System.in);
     }
 }
